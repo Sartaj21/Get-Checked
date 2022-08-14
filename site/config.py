af5da372bd258de25f7d14a33f89823a09f060c7
Sartaj GIll <1sartajgill@gmail.com>
2022-08-14 10:43:21 -0400

    hid config.py

--- site/config.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "4bckTJxx597EpwAgsVPB4jaZb" #api key
-consumer_secret = "Gayo42TGGDFLz8ch6hPUJkY9Kjcv9KPi5kALDjcFoMMn30PlgS" #api key secret
-access_token = "1558230505165824002-Vbimdu2lv7CyBHFuZYVbm8qb8lArVI"
-access_token_secret = "PTPU5l7S3MydhTrcLXaoG52wvcwhExRF83u4NisDAtaBz"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAPOdfwEAAAAAp498eSCHq4PudtLysRj0jSDFd2Q%3Dqs4lKuGovdEJxWehNIdFBF1qWqxHmyviIOiu60QXONgdo6LGtQ"
